7de9ecf765a0c7107e371d5bc3d278e34c7fcdad
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-16 18:54:51 +0100

    feat\ route and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { StudentAssignmentsPage } from "./components/StudentAssignmentsPage";
-import ClassesList from "./components/ClassesList";
-import { Header } from "./components/Header";
-import { Navbar } from "./components/Navbar";
-import { DisplayStudentAssignment } from "./components/DisplayStudentAssignment";
-import { StudentLessonsPage } from "./components/StudentLessonsPage";
-
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { StudentAssignmentsPage } from './components/StudentAssignmentsPage';
+import ClassesList from './components/ClassesList';
+import { Header } from './components/Header';
+import { Navbar } from './components/Navbar';
+import { DisplayStudentAssignment } from './components/DisplayStudentAssignment';
+import { StudentLessonsPage } from './components/StudentLessonsPage';
+import Login from './components/Login';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
+  console.log('🚀 ~ App ~ user:', user);
+
+  const handleLogin = (email) => {
+    setUser(email);
+  };
   return (
     <>
       {/* <Header /> Most likely used for home page/login */}
       <Navbar />
       <Routes>
         <Route
-          path="/student/assignments"
+          path='/student/assignments'
           element={<StudentAssignmentsPage />}
         />
-        <Route path="/teachers/home" element={<ClassesList />} />
+        <Route path='/teachers/home' element={<ClassesList />} />
         <Route
-          path="/student/assignments/:assignment_id"
+          path='/student/assignments/:assignment_id'
           element={<DisplayStudentAssignment />}
         />
-        <Route path="/student/lessons" element={<StudentLessonsPage/>}/>
+        <Route path='/student/lessons' element={<StudentLessonsPage />} />
+        <Route path='/login' element={<Login handleLogin={handleLogin} />} />
       </Routes>
     </>
   );
