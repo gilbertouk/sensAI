f68cbf1fab4a09696b479c38dcf9b0bcd179197a
RonaldB123 <ronaldbeck@hotmail.co.uk>
2023-09-21 16:14:57 +0100

    added links to work/lesson buttons

--- src/components/TeacherLessonsButton.jsx
+++ src/components/TeacherLessonsButton.jsx
@@ -1,12 +1,16 @@
 import { Button } from "@mui/material"
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
-
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const TeacherLessonsButton = () => {
+    const { class_id } = useParams();
     return (
+        <Link to={`/teacher/lessons/${class_id}`}>
         <Button variant="contained" startIcon={<LocalLibraryIcon/>} sx={{ml: 2}}>
             Lessons Assigned
         </Button>
+        </Link>
     )
     
 }
--- src/components/TeacherWorkButton.jsx
+++ src/components/TeacherWorkButton.jsx
@@ -1,13 +1,17 @@
 import { Button } from "@mui/material"
 import AssignmentIcon from '@mui/icons-material/Assignment';
-
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export const TeacherWorkButton = () => {
+    const { class_id } = useParams();
     return (
+        <Link to={`/teacher/assignments/${class_id}`}>
         <Button variant="contained" startIcon={<AssignmentIcon/>} sx={{mr: 2}}>
             Work Assigned
         </Button>
+        </Link>
     )
     
 }
