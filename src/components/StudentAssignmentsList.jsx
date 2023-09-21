de2d378e2174836fa4dfeead499642fec99844d2
Gilberto Silva <gilbertoantoniouk@gmail.com>
2023-09-21 12:21:48 +0100

    fixed error with StudentAssignmentsList component

--- src/components/StudentAssignmentsList.jsx
+++ src/components/StudentAssignmentsList.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAssignmentByStudentId } from "../utils/api";
