e99bb3dfee4a2747f9e640fb031ed3f091136522
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-22 13:15:52 +0100

    MUI styling and loading skeleton

--- src/components/TeacherAssignmentsNew.jsx
+++ src/components/TeacherAssignmentsNew.jsx
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { postAssignment, getClassesByTeacherID } from "../utils/api";
 
+import {
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  CircularProgress,
+  Container,
+  Grid,
+  Typography,
+  Skeleton,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+
 const TeacherAssignmentsNew = ({ user }) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -8,6 +22,7 @@ const TeacherAssignmentsNew = ({ user }) => {
   const [classes, setClasses] = useState([]);
   const [selectedClass, setSelectedClass] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [posting, setPosting] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -32,60 +47,123 @@ const TeacherAssignmentsNew = ({ user }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (user && user.id && selectedClass) {
+      setPosting(true);
       postAssignment(user.id, selectedClass, title, body, dueDate)
         .then((data) => console.log("Assignment Posted:", data))
-        .catch((err) => console.error("Error Posting Assignment:", err));
+        .catch((err) => console.error("Error Posting Assignment:", err))
+        .finally(() => {
+          setPosting(false);
+        });
     } else {
       console.error("User ID or Selected Class ID not available");
     }
   };
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <Container component="main" maxWidth="sm">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={56} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={56} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={118} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={56} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={56} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" height={56} />
+          </Grid>
+        </Grid>
+      </Container>
+    );
   return (
-    <div>
-      <h2>Teacher Assignments - New</h2>
+    <Container component="main" maxWidth="sm">
+      <Typography variant="h5" component="h2" gutterBottom>
+        Create New Assignment
+      </Typography>
       <form onSubmit={handleSubmit}>
-        <label>
-          Class:
-          <select
-            value={selectedClass}
-            onChange={(e) => setSelectedClass(e.target.value)}
-            required
-          >
-            {classes.map((cls) => (
-              <option key={cls.id} value={cls.id}>
-                {cls.name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label>
-          Title:
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Body:
-          <textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Due Date:
-          <input
-            type="date"
-            value={dueDate}
-            onChange={(e) => setDueDate(e.target.value)}
-          />
-        </label>
-        <button type="submit">Post Assignment</button>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Title"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Body"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              required
+              multiline
+              rows={5}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Due Date"
+              type="date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+              InputLabelProps={{ shrink: true }}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel
+                id="class-label"
+                sx={{
+                  backgroundColor: "white",
+                  paddingLeft: "6px",
+                  paddingRight: "6px",
+                }}
+              >
+                Class
+              </InputLabel>
+              <Select
+                labelId="class-label"
+                value={selectedClass}
+                onChange={(e) => setSelectedClass(e.target.value)}
+                label="Class" // Required for outlined variant
+                required
+              >
+                {classes.map((cls) => (
+                  <MenuItem key={cls.id} value={cls.id}>
+                    {cls.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              loading={posting}
+            >
+              Post Assignment
+            </LoadingButton>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Container>
   );
 };
 
