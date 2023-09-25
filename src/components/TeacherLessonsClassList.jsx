152be57e5b2ed37cc95d890f47fbd6508562fa90
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-25 12:20:10 +0100

    create lesson button added

--- src/components/TeacherLessonsClassList.jsx
+++ src/components/TeacherLessonsClassList.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { getLessonByTeacherAndClass } from '../utils/api';
 import { TeacherLessonCard } from './TeacherLessonCard';
-import { Link } from 'react-router-dom';
+import LoadingButton from "@mui/lab/LoadingButton";
+import Box from '@mui/material/Box';
 
 const TeacherLessonsClassList = ({ user }) => {
   const [lessons, setLessons] = useState(null);
   const [loading, setLoading] = useState(true);
   const { class_id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
@@ -19,10 +21,30 @@ const TeacherLessonsClassList = ({ user }) => {
     }
   }, [user]);
 
+  function handleLessonToCreate() {
+    navigate(`/teacher/lessons/new`);
+  }
+
   return loading ? (
     <p>loading...</p>
   ) : (
     <ul className="student-lesson-container">
+        <Box mt={3}>
+        </Box> 
+        <Box display="flex" justifyContent="center">
+            <Box width="33%">
+                <LoadingButton 
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      handleLessonToCreate();
+                    }}
+                    >Create new lesson
+                </LoadingButton>
+            </Box>
+        </Box>
             {lessons.map(lesson => {
                 return (
                 <div key={lesson.id}>
