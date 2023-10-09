5cf31ca4ff2dbe8a9c9c953215d6a5c0fb7bafe6
Gilberto Silva <gilbertoantoniouk@gmail.com>
2023-10-09 15:31:41 +0100

    change readme and firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = 
-{
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_KEY,
   authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_DOMAIN,
   projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT,
   storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_BUCKET,
   messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_SENDER,
   appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP,
-}
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
